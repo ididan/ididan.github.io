d086bbfdc59dac973aad04702a993e2555cb56cc
ididan <ididan@paran.com>
2021-07-05 00:30:36 +0900

    Get quote from rest api

--- js/quotes.js
+++ js/quotes.js
@@ -1,30 +1,11 @@
-const quotes = [
-    {
-        qoute: "q 1",
-        author: "a 1",
-    },
-    {
-        qoute: "q 2",
-        author: "a 2",
-    },
-    {
-        qoute: "q 3",
-        author: "a 3",
-    },
-    {
-        qoute: "q 4",
-        author: "a 4",
-    },
-    {
-        qoute: "q 5",
-        author: "a 5",
-    },
-]
-
-const quote = document.querySelector("#quote span:first-child");
-const author = document.querySelector("#quote span:last-child");
-
-const random = Math.floor(Math.random() * quotes.length)
-const todayQuote = quotes[random];
-quote.innerText = todayQuote.qoute;
-author.innerText = todayQuote.author;
+const url = "https://quotes.rest/qod";
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const quote = document.querySelector("#quote span:first-child");
+        const author = document.querySelector("#quote span:last-child");
+        const q = data.contents.quotes[0];
+        quote.innerText = q.quote;
+        author.innerText = q.author;
+        console.log(data);
+    });
--- js/weather.js
+++ js/weather.js
@@ -5,13 +5,14 @@ const locationPosition = navigator.geolocation.getCurrentPosition(
         const lat = position.coords.latitude;
         const lng = position.coords.longitude;
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
-        console.log(url);
-        fetch(url).then(response => response.json()).then(data => {
-            const weather = document.querySelector("#weather span:first-child");
-            const city = document.querySelector("#weather span:last-child");
-            city.innerText = data.name;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-        });
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const weather = document.querySelector("#weather span:first-child");
+                const city = document.querySelector("#weather span:last-child");
+                city.innerText = data.name;
+                weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            });
     },
     () => {
         alert("Can't find you. No weather for you.")
