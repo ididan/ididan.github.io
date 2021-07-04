147d295fe1c8e98c177af304070553b42d8c84fc
ididan <ididan@paran.com>
2021-07-04 16:06:54 +0900

    Add login form and greeting msg and local storage

--- src/app.js
+++ src/app.js
@@ -4,25 +4,41 @@ const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-const login = (event)=>{
+const login = (event) => {
     // form 에서 submit 될때 browser 가 refresh 되는 기본 동작을 막아줌
     event.preventDefault();
     console.dir(event);
     // 위에서 막아서 username 을 우리가 처리할수 있도록 함
     const username = loginInput.value;
     console.log(username);
-    greeting.innerText = `Hello ${username}`;
 
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+    localStorage.setItem(USERNAME_KEY, username);
+    showGreetings(username);
 }
-loginForm.onsubmit = login
 
 const link = document.querySelector("a")
-link.onclick = (event)=>{
+link.onclick = (event) => {
     event.preventDefault();
     console.log(event);
-    
+
     //alert("clicked!");
+}
+
+const username = localStorage.getItem(USERNAME_KEY);
+console.log(username);
+
+const showGreetings = (username)=>{
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+if (username === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.onsubmit = login
+} else {
+    showGreetings(username);
 }
