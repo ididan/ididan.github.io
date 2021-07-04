f3b11f0aa3afc50ac673b2a6c897d3f2f932511f
ididan <ididan@paran.com>
2021-07-05 01:11:34 +0900

    Change to random background image when reload

--- js/background.js
+++ js/background.js
@@ -1,11 +1,12 @@
 // https://www.pexels.com/api/documentation/
 const API_KEY = "563492ad6f91700001000001d53d7f62576d44149d3aa90dec0b218e";
 
-const imageUrl = "https://api.pexels.com/v1/search?query=nature&per_page=1"
+const imageUrl = "https://api.pexels.com/v1/search?query=nature"
 fetch(imageUrl)
     .then(response => response.json())
     .then(data => {
-        const url = data.photos[0].src.original;
+        const index = Math.floor(Math.random() * data.photos.length);
+        const url = data.photos[index].src.original;
 
         document.body.style.background = `no-repeat center/100% url("${url}")`;
     });
