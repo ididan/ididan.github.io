3b3937cedd43980c6e83b7d7c68408eb6abeb386
ididan <ididan@paran.com>
2021-07-04 19:46:19 +0900

    Add todo removes

--- js/todo.js
+++ js/todo.js
@@ -2,17 +2,28 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+const TODO_KEY = "todos";
+
+let todos = [];
+const saveTodos = () => {
+    localStorage.setItem(TODO_KEY, JSON.stringify(todos));
+};
 const updateTodo = (newTodo) => {
-    console.log(newTodo);
     const li = document.createElement("li");
-    
+
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
+    li.id = newTodo.id;
     li.appendChild(span);
 
     const button = document.createElement("button");
     button.innerText = "❌";
-    button.onclick = (event) => event.target.parentElement.remove();
+    button.onclick = (event) => {
+        const li = event.target.parentElement;
+        todos = todos.filter(item => item.id !== parseInt(li.id));
+        saveTodos();
+        li.remove();
+    };
     li.appendChild(button);
 
     todoList.appendChild(li);
@@ -20,7 +31,21 @@ const updateTodo = (newTodo) => {
 
 todoForm.onsubmit = (event) => {
     event.preventDefault();
-    const newTodo = todoInput.value;
+    const newTodo = {
+        id: Date.now(),
+        text: todoInput.value,
+    };
     updateTodo(newTodo);
     todoInput.value = "";
+
+    todos.push(newTodo);
+    saveTodos();
 }
+
+const savedTodos = localStorage.getItem(TODO_KEY);
+if (savedTodos != null) {
+    todos = JSON.parse(savedTodos);
+    todos.forEach(element => {
+        updateTodo(element);
+    });
+};
